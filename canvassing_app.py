cbc36947e535a7ee0f94868da1b2188635711ad6
ethan <16707449+peregrinethan@users.noreply.github.com>
2022-06-10 09:30:18 -0500

    Add files via upload

--- canvassing_app.py
+++ canvassing_app.py
@@ -83,31 +83,33 @@ if address:
 
                 st.pydeck_chart(pdk.Deck(
                      map_style='mapbox://styles/mapbox/light-v9',
-                     # initial_view_state=pdk.ViewState(
-                     #     latitude=37.76,
-                     #     longitude=-122.4,
-                     #     zoom=11,
-                     #     pitch=50,
-                     # ),
+                     initial_view_state=pdk.ViewState(
+                         latitude=df_canvass['lat'].mean(),
+                         longitude=-df_canvass['lon'].mean(),
+                         zoom=11,
+                         pitch=50,
+                     ),
                      layers=[
-                         pdk.Layer(
-                            'HexagonLayer',
-                            data=df_canvass,
-                            get_position='[lon, lat]',
-                            radius=200,
-                            elevation_scale=4,
-                            elevation_range=[0, 1000],
-                            pickable=True,
-                            extruded=True,
-                         ),
+                         # pdk.Layer(
+                         #    'HexagonLayer',
+                         #    data=df_canvass,
+                         #    get_position='[lon, lat]',
+                         #    radius=200,
+                         #    elevation_scale=4,
+                         #    elevation_range=[0, 1000],
+                         #    pickable=True,
+                         #    extruded=True,
+                         # ),
                          pdk.Layer(
                              'ScatterplotLayer',
                              data=df_canvass,
                              get_position='[lon, lat]',
                              get_color='[200, 30, 0, 160]',
-                             get_radius=200,
+                             get_radius=20,
+                             opacity=0.5,
                          ),
                      ],
+                     tooltip={"text": "{address} {city}, TX {zip}\n{unit_type} {unit}"}
                  ))
 
 
