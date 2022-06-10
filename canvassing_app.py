020d6bcc943c8e20e714126b5eac173dd889d9b9
ethan <16707449+peregrinethan@users.noreply.github.com>
2022-06-10 17:19:37 -0500

    Add files via upload

--- canvassing_app.py
+++ canvassing_app.py
@@ -34,7 +34,7 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 ls = []
 for row in rows:
     # ls = ls.append(row.emails)
-    st.write(row)
+    st.write(row['emails'])
 
 
 def check_email():
